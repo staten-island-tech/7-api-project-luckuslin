b95ae12d0db99c95e85d9706bd49aadf5fffc770
luckuslin <lukasl18@nycstudents.net>
2026-04-15 09:24:20 -0400

    Update djfsdjhggfg.py

--- djfsdjhggfg.py
+++ djfsdjhggfg.py
@@ -1,18 +1,11 @@
 import requests
 
-def getPoke(poke):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+def clash(card):
+    response = requests.get(f"https://github.com/martincarrera/clash-royale-api.git")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
-    return {
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    }
-
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+    print (data)
+clash("D")
