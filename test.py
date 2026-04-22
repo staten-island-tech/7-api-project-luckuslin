cd6871ddecb0c8c398c944f4ae919a1bcad008fa
luckuslin <lukasl18@nycstudents.net>
2026-04-22 09:24:10 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
-
+import random
 from balldontlie import BalldontlieAPI
 
 
@@ -10,8 +10,8 @@ headers = {"Authorization": "f9eb4f44-79b4-48c2-9e40-38d25c59b741"}
 response = requests.get(url, headers=headers)
 data = response.json()
 
-datas=[
-    {'id': 1, 'conference': 'East', 'division': 'Southeast', 'city': 'Atlanta', 'name': 'Hawks', 'full_name': 'Atlanta Hawks', 'abbreviation': 'ATL'}
+teams=[
+    {'id': 1, 'conference': 'East', 'division': 'Southeast', 'city': 'Atlanta', 'name': 'Hawks', 'full_name': 'Atlanta Hawks', 'abbreviation': 'ATL'},
     {'id': 2, 'conference': 'East', 'division': 'Atlantic', 'city': 'Boston', 'name': 'Celtics', 'full_name': 'Boston Celtics', 'abbreviation': 'BOS'}, 
     {'id': 3, 'conference': 'East', 'division': 'Atlantic', 'city': 'Brooklyn', 'name': 'Nets', 'full_name': 'Brooklyn Nets', 'abbreviation': 'BKN'},
     {'id': 4, 'conference': 'East', 'division': 'Southeast', 'city': 'Charlotte', 'name': 'Hornets', 'full_name': 'Charlotte Hornets', 'abbreviation': 'CHA'},
@@ -28,6 +28,23 @@ datas=[
     {'id': 15, 'conference': 'West', 'division': 'Southwest', 'city': 'Memphis', 'name': 'Grizzlies', 'full_name': 'Memphis Grizzlies', 'abbreviation': 'MEM'},
     {'id': 16, 'conference': 'East', 'division': 'Southeast', 'city': 'Miami', 'name': 'Heat', 'full_name': 'Miami Heat', 'abbreviation': 'MIA'},
     {'id': 17, 'conference': 'East', 'division': 'Central', 'city': 'Milwaukee', 'name': 'Bucks', 'full_name': 'Milwaukee Bucks', 'abbreviation': 'MIL'},
+    {'id': 18, 'conference': 'West', 'division': 'Northwest', 'city': 'Minnesota', 'name': 'Timberwolves', 'full_name': 'Minnesota Timberwolves', 'abbreviation': 'MIN'},
+    {'id': 19, 'conference': 'West', 'division': 'Southwest', 'city': 'New Orleans', 'name': 'Pelicans', 'full_name': 'New Orleans Pelicans', 'abbreviation': 'NOP'},
+    {'id': 20, 'conference': 'East', 'division': 'Atlantic', 'city': 'New York', 'name': 'Knicks', 'full_name': 'New York Knicks', 'abbreviation': 'NYK'},
+    {'id': 21, 'conference': 'West', 'division': 'Northwest', 'city': 'Oklahoma City', 'name': 'Thunder', 'full_name': 'Oklahoma City Thunder', 'abbreviation': 'OKC'},
+    {'id': 22, 'conference': 'East', 'division': 'Southeast', 'city': 'Orlando', 'name': 'Magic', 'full_name': 'Orlando Magic', 'abbreviation': 'ORL'},
+    {'id': 23, 'conference': 'East', 'division': 'Atlantic', 'city': 'Philadelphia', 'name': '76ers', 'full_name': 'Philadelphia 76ers', 'abbreviation': 'PHI'},
+    {'id': 24, 'conference': 'West', 'division': 'Pacific', 'city': 'Phoenix', 'name': 'Suns', 'full_name': 'Phoenix Suns', 'abbreviation': 'PHX'},
+    {'id': 25, 'conference': 'West', 'division': 'Northwest', 'city': 'Portland', 'name': 'Trail Blazers', 'full_name': 'Portland Trail Blazers', 'abbreviation': 'POR'},
+    {'id': 26, 'conference': 'West', 'division': 'Pacific', 'city': 'Sacramento', 'name': 'Kings', 'full_name': 'Sacramento Kings', 'abbreviation': 'SAC'},
+    {'id': 27, 'conference': 'West', 'division': 'Southwest', 'city': 'San Antonio', 'name': 'Spurs', 'full_name': 'San Antonio Spurs', 'abbreviation': 'SAS'},
+    {'id': 28, 'conference': 'East', 'division': 'Atlantic', 'city': 'Toronto', 'name': 'Raptors', 'full_name': 'Toronto Raptors', 'abbreviation': 'TOR'},
+    {'id': 29, 'conference': 'West', 'division': 'Northwest', 'city': 'Utah', 'name': 'Jazz', 'full_name': 'Utah Jazz', 'abbreviation': 'UTA'},
+    {'id': 30, 'conference': 'East', 'division': 'Southeast', 'city': 'Washington', 'name': 'Wizards', 'full_name': 'Washington Wizards', 'abbreviation': 'WAS'},
+
+]
 
 
-    ]
+print(teams[3]['conference'])
+    
+    
